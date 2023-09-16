6ccd0ed36c8b4cd1702f026f07649703935f2b5f
Mona Lisa Octocat <bernkra@gmail.com>
2023-09-16 13:22:17 +0200

    Update function

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import { useState } from 'react';
 export default function App() {
   // User pamplate input
 
-  const regx = /[\w\d\s]+/;
+  // const regx = /[\w\d\s]+/;
 
   const startUrl = 'https://api.memegen.link/images/';
 
   const [imageInput, setImageInput] = useState('ackbar');
-
+  // const [imageTestInput, setImageTestInput] = useState('ackbar');
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
   const [imageInputMode, setImageInputMode] = useState(
     startUrl + imageInput + '.jpg',
   );
   // const baseUrl = startUrl + imageInput + '.jpg';
-  let lodeUrl = imageInputMode;
-
-  const imagteTestUrl = 'https://api.memegen.link/images/' + imageInput;
+  // let lodeUrl = imageInputMode;
 
   const testUrl =
     'https://api.memegen.link/images/' +
@@ -29,69 +27,70 @@ export default function App() {
     '/' +
     bottomText +
     '.jpg';
-  const testUrlTop =
-    'https://api.memegen.link/images/' + imageInput + '/' + topText + '.jpg';
+  // const testUrlTop =
+  // 'https://api.memegen.link/images/' + imageInput + '/' + topText + '.jpg';
 
-  const testUrlbottom =
-    'https://api.memegen.link/images/' +
-    imageInput +
-    '/_/' +
-    bottomText +
-    '.jpg';
+  // const testUrlbottom =
+  // 'https://api.memegen.link/images/' +
+  // imageInput +
+  // '/_/' +
+  // bottomText +
+  // '.jpg';
 
   function saveImage() {
     FileSaver.saveAs(imageInputMode, 'image.jpg');
   }
   function lode() {
-    return setImageInputMode(lodeUrl);
+    return setImageInputMode(testUrl);
   }
-  function inputTopText() {
-    if (regx.test(topText) && regx.test(bottomText)) {
-      lodeUrl = testUrl;
-      return setImageInputMode(testUrl);
-    }
-    if (regx.test(topText)) {
-      lodeUrl = testUrlTop;
-      return setImageInputMode(testUrlTop);
-    }
-    if (regx.test(bottomText)) {
-      lodeUrl = testUrlbottom;
-      return setImageInputMode(testUrlbottom);
-    }
-  }
+  // function inputTopText() {
+  // if (regx.test(topText) && regx.test(bottomText)) {
+  // lodeUrl = testUrl;
+  // return setImageInputMode(testUrl);
+  // }
+  //  if (regx.test(topText)) {
+  // lodeUrl = testUrlTop;
+  // return setImageInputMode(testUrlTop);
+  // }
+  // if (regx.test(bottomText)) {
+  // lodeUrl = testUrlbottom;
+  //  return setImageInputMode(testUrlbottom);
+  // }
+  // }
 
   return (
     <>
       <h1>Meme Generator</h1>
 
       <label>
-        Meme template Top text
+        Meme template
+        <input
+          value={imageInput}
+          onChange={(e) => setImageInput(e.target.value)}
+          onKeyUp={lode}
+        />
+      </label>
+      <div>{imageInputMode}</div>
+      <div>
+        <img src={imageInputMode} alt=" meme" data-test-id="meme-image" />
+      </div>
+      <label>
+        Top text
         <input
           value={topText}
           onChange={(e) => setTopText(e.target.value)}
-          onKeyUp={inputTopText}
+          onKeyUp={lode}
         />
       </label>
-
-      <div>
-        <img src={imageInputMode} alt=" meme" data-test-id="meme-image" />
-      </div>
-
-      <div>{imageInputMode}</div>
-
-      <div>
-        <img src={imageInputMode} alt=" meme" data-test-id="meme-image" />
-      </div>
 
       <label>
         Bottom text
         <input
           value={bottomText}
           onChange={(e) => setBottomText(e.target.value)}
-          onKeyUp={inputTopText}
+          onKeyUp={lode}
         />
       </label>
-
       <button onClick={saveImage}>Download</button>
     </>
   );
